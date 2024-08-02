28012c3c452a0563c7757d5db8c2462e9711b98c
Jaiden W <41570385+JaidenWU@users.noreply.github.com>
2024-08-02 14:16:50 +1000

    created linesensor class

--- WheelTest/WheelTest.ino
+++ WheelTest/WheelTest.ino
@@ -1,7 +1,7 @@
 #include "Wheel.h"
 
-#define LEFT_WHEEL_PIN 11
-#define RIGHT_WHEEL_PIN 10
+#define LEFT_WHEEL_PIN 10
+#define RIGHT_WHEEL_PIN 9
 
 Servo leftServo;
 Servo rightServo;
@@ -11,10 +11,11 @@ Wheel rightWheel (rightServo,RIGHT_WHEEL_PIN);
 
 
 void setup() {
+  Serial.begin(9600);  
   leftWheel.initLeftWheel();
   rightWheel.initRightWheel();
 }
 
 void loop() {
-  
+  leftWheel.moveForward();
 }
