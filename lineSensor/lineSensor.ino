a8dd0e6248ddb0000c1c472c44a7d7e676135c3c
Jaiden Wu <jaiden.wu@education.nsw.gov.au>
2024-08-11 17:29:14 +1000

    added ledArray to work with lineSensor
    - should display happy face if on line
    - should display sad face if not on line

--- lineSensor/lineSensor.ino
+++ lineSensor/lineSensor.ino
@@ -1,20 +1,31 @@
 #include "LineSensor.h"
+#include "LEDArray.h"
 
 #define LEFT_LINESENSOR_PIN 13
 #define RIGHT_LINESENSOR_PIN 3
 
 LineSensor lineSensor(LEFT_LINESENSOR_PIN, RIGHT_LINESENSOR_PIN);
+ArduinoLEDMatrix matrix;  
+LEDArray ledArray(matrix);  
+
 
 void setup() {
   Serial.begin(9600);
   lineSensor.init();
+  ledArray.init();
 }
 
 void loop() {
-  if (lineSensor.determineState()) {
+  bool isFollowingLine = lineSensor.determineState();
+
+  if (isFollowingLine) {
     Serial.println("Following the line");
   } else {
     Serial.println("Lost the line");
   }
+
+   // Update the LED array display based on the state
+  ledArray.updateDisplay(isFollowingLine);
+  
   delay(500); 
 }
