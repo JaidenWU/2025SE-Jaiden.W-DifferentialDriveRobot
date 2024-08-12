746a2faddc2ac86dd81513d4e2099d7a50d0fdc3
Jaiden W <41570385+JaidenWU@users.noreply.github.com>
2024-08-12 14:16:57 +1000

    testing lineSensor

--- lineSensor/lineSensor.ino
+++ lineSensor/lineSensor.ino
@@ -11,7 +11,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(lineSensor.readLeftLineSensor());
+  Serial.println(lineSensor.readRightLineSensor());  //L45 black, 35 white    39 black 24 whitee
   //if (lineSensor.determineState()) {
     //Serial.println("Following the line");
   //} else {
